0fe33db2480fbe807ee05080004b7ceafa281059
boris-pjr <89574312+boris-pjr@users.noreply.github.com>
2024-05-28 17:32:11 +0200

    Affichage des travaux par catégorie

--- FrontEnd/assets/script/index.js
+++ FrontEnd/assets/script/index.js
@@ -1,7 +1,8 @@
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-// RECUPETATION DES TRAVAUX DEPUIS LE BACK-END //
+// RECUPERATION DES TRAVAUX DEPUIS LE BACK-END //
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
+let allWorks = [];
+let allCategories = [{ id: 0, name: "Tous" }];
 // Récupération des travaux depuis l'API
 fetch("http://localhost:5678/api/works")
   .then((response) => {
@@ -46,4 +47,52 @@ function creationProjet(projet) {
   figureElement.appendChild(titleElement);
 
   return figureElement;
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// REALISATION DES FILTRES DES TRAVAUX : AFFICHAGE PAR CATEGORIE //
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// Récupération des catégories depuis l'API
+fetch("http://localhost:5678/api/categories")
+  .then((response) => {
+    if (response.status === 200) {
+      console.log(response);
+      return response.json();
+    }
+  })
+  .then((data) => {
+    if (data) {
+      data.forEach((element) => {
+        allCategories.push(element);
+      });
+      console.log(allCategories);
+      affichageCategories(allCategories);
+    }
+  });
+
+// Fonction pour afficher les catégories sous forme de boutons de filtre
+function affichageCategories(categories) {
+  const filtresCategories = document.querySelector(".filtres");
+  categories.forEach((uniqueCategorie) => {
+    const boutonCategorie = document.createElement("button");
+    boutonCategorie.id = uniqueCategorie.id;
+    boutonCategorie.textContent = uniqueCategorie.name;
+    boutonCategorie.className = "filter";
+
+    boutonCategorie.addEventListener("click", () =>
+      filtrageCategorie(uniqueCategorie.id)
+    );
+    filtresCategories.appendChild(boutonCategorie);
+  });
+}
+
+// Fonction pour filtrer les projets par catégorie
+function filtrageCategorie(categoryId) {
+  let allFigures = document.querySelectorAll(".figure");
+  allFigures.forEach((element) => {
+    if (element.dataset.idCategory == categoryId || categoryId == 0)
+      element.style.display = "block";
+    else element.style.display = "none";
+  });
 }
