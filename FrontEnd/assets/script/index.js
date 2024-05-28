3b223a89fa62c61f78826ca398e06d05a6a90e66
boris-pjr <89574312+boris-pjr@users.noreply.github.com>
2024-05-28 17:09:47 +0200

    Affichage des travaux dans la galerie

--- FrontEnd/assets/script/index.js
+++ FrontEnd/assets/script/index.js
@@ -14,6 +14,36 @@ fetch("http://localhost:5678/api/works")
   .then((data) => {
     if (data) {
       allWorks = data;
+      affichageWorks(data);
       console.log(data);
     }
   });
+
+// Fonction pour afficher les travaux dans la galerie
+function affichageWorks(works) {
+  const galerie = document.querySelector(".gallery");
+  galerie.innerHTML = "";
+  for (let i = 0; i < works.length; i++) {
+    const projet = works[i];
+    const figureElement = creationProjet(projet);
+    galerie.appendChild(figureElement);
+  }
+}
+// Fonction pour créer un élément de projet
+function creationProjet(projet) {
+  const figureElement = document.createElement("figure");
+  figureElement.id = "figure" + projet.id;
+  figureElement.dataset.idCategory = projet.categoryId;
+  figureElement.classList.add("figure");
+
+  const imageElement = document.createElement("img");
+  const titleElement = document.createElement("figcaption");
+
+  imageElement.src = projet.imageUrl;
+  titleElement.innerText = projet.title;
+
+  figureElement.appendChild(imageElement);
+  figureElement.appendChild(titleElement);
+
+  return figureElement;
+}
